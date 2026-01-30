6e3ab301c82b306587a1148e210ae7b66a4001f1
Javi Llinares <javillinares@MacBook-Air.local>
2026-01-30 06:36:16 +0100

    fix: refine SW static assets pathing for silent boot

--- public/sw.js
+++ public/sw.js
@@ -3,11 +3,11 @@ const CACHE_NAME = `socdepoble-${CACHE_VERSION}`;
 const HEALTH_CHECK_INTERVAL = 5 * 60 * 1000; // 5 minutes
 
 const STATIC_ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png'
+    './',
+    './manifest.json',
+    './favicon.png',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 // Install event - cache static assets individually for maximum resilience
